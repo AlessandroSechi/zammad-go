88108e32f9faf4c6fdf9471cb3a92dd8015d7e51
Miek Gieben <miek@miek.nl>
2024-07-09 22:10:56 +0200

    Use docs to fill out online notification.
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- online_notification.go
+++ online_notification.go
@@ -1,54 +1,67 @@
 package zammad
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-// OnlineNotification represent a Zammad online notification.
+// OnlineNotification represent a Zammad online notification. See https://docs.zammad.org/en/latest/api/notification.html.
 type OnlineNotification struct {
+	ID             int       `json:"id"`
+	OID            int       `json:"o_id"`
+	ObjectLookupID int       `json:"object_lookup_id"`
+	TypeLookupID   int       `json:"type_lookup_id"`
+	UserID         int       `json:"user_id"`
+	Seen           bool      `json:"seen"`
+	UpdatedByID    int       `json:"updated_by_id"`
+	CreatedByID    int       `json:"created_by_id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
 
-func (c *Client) OnlineNotificationList() (*[]map[string]interface{}, error) {
-	var notifications []map[string]interface{}
+func (c *Client) OnlineNotificationList() ([]OnlineNotification, error) {
+	var notifications []OnlineNotification
 
 	req, err := c.NewRequest("GET", fmt.Sprintf("%s%s", c.Url, "/api/v1/online_notifications"), nil)
 	if err != nil {
-		return &notifications, err
+		return notifications, err
 	}
 
-	if err = c.SendWithAuth(req, &notifications); err != nil {
-		return &notifications, err
+	if err = c.SendWithAuth(req, notifications); err != nil {
+		return notifications, err
 	}
 
-	return &notifications, nil
+	return notifications, nil
 }
 
-func (c *Client) OnlineNotificationShow(notificationID int) (*map[string]interface{}, error) {
-	var notification map[string]interface{}
+func (c *Client) OnlineNotificationShow(notificationID int) (OnlineNotification, error) {
+	var notification OnlineNotification
 
 	req, err := c.NewRequest("GET", fmt.Sprintf("%s%s", c.Url, fmt.Sprintf("/api/v1/online_notifications/%d", notificationID)), nil)
 	if err != nil {
-		return &notification, err
+		return notification, err
 	}
 
-	if err = c.SendWithAuth(req, &notification); err != nil {
-		return &notification, err
+	if err = c.SendWithAuth(req, notification); err != nil {
+		return notification, err
 	}
 
-	return &notification, nil
+	return notification, nil
 }
 
-func (c *Client) OnlineNotificationUpdate(notificationID int, n *map[string]interface{}) (*map[string]interface{}, error) {
-	var notification map[string]interface{}
+func (c *Client) OnlineNotificationUpdate(notificationID int, n OnlineNotification) (OnlineNotification, error) {
+	var notification OnlineNotification
 
 	req, err := c.NewRequest("PUT", fmt.Sprintf("%s%s", c.Url, fmt.Sprintf("/api/v1/online_notifications/%d", notificationID)), n)
 	if err != nil {
-		return &notification, err
+		return notification, err
 	}
 
-	if err = c.SendWithAuth(req, &notification); err != nil {
-		return &notification, err
+	if err = c.SendWithAuth(req, notification); err != nil {
+		return notification, err
 	}
 
-	return &notification, nil
+	return notification, nil
 }
 
 func (c *Client) OnlineNotificationDelete(notificationID int) error {
