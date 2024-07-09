44ba293afd757c2599b0e05244ef69a38a234047
Miek Gieben <miek@miek.nl>
2024-07-09 22:26:56 +0200

    At least define an Object type
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- object.go
+++ object.go
@@ -7,66 +7,66 @@ import "fmt"
 //
 //	Adjusting objects via API can cause serious issues with your instance. Proceed with absolute caution and ensure
 //	to adjust any of Zammads default fields.
-type Object struct{}
+type Object *map[string]interface{}
 
-func (c *Client) ObjectList() (*[]map[string]interface{}, error) {
-	var objects []map[string]interface{}
+func (c *Client) ObjectList() ([]Object, error) {
+	var objects []Object
 
 	req, err := c.NewRequest("GET", fmt.Sprintf("%s%s", c.Url, "/api/v1/object_manager_attributes"), nil)
 	if err != nil {
-		return &objects, err
+		return objects, err
 	}
 
-	if err = c.SendWithAuth(req, &objects); err != nil {
-		return &objects, err
+	if err = c.SendWithAuth(req, objects); err != nil {
+		return objects, err
 	}
 
-	return &objects, nil
+	return objects, nil
 }
 
-func (c *Client) ObjectShow(objectID int) (*map[string]interface{}, error) {
-	var object map[string]interface{}
+func (c *Client) ObjectShow(objectID int) (Object, error) {
+	var object Object
 
 	req, err := c.NewRequest("GET", fmt.Sprintf("%s%s", c.Url, fmt.Sprintf("/api/v1/object_manager_attributes/%d", objectID)), nil)
 	if err != nil {
-		return &object, err
+		return object, err
 	}
 
-	if err = c.SendWithAuth(req, &object); err != nil {
-		return &object, err
+	if err = c.SendWithAuth(req, object); err != nil {
+		return object, err
 	}
 
-	return &object, nil
+	return object, nil
 }
 
-func (c *Client) ObjectCreate(o *map[string]interface{}) (*map[string]interface{}, error) {
-	var object map[string]interface{}
+func (c *Client) ObjectCreate(o Object) (Object, error) {
+	var object Object
 
 	req, err := c.NewRequest("POST", fmt.Sprintf("%s%s", c.Url, "/api/v1/object_manager_attributes"), o)
 	if err != nil {
-		return &object, err
+		return object, err
 	}
 
-	if err = c.SendWithAuth(req, &object); err != nil {
-		return &object, err
+	if err = c.SendWithAuth(req, object); err != nil {
+		return object, err
 	}
 
-	return &object, nil
+	return object, nil
 }
 
-func (c *Client) ObjectUpdate(objectID int, o *map[string]interface{}) (*map[string]interface{}, error) {
-	var object map[string]interface{}
+func (c *Client) ObjectUpdate(objectID int, o Object) (Object, error) {
+	var object Object
 
 	req, err := c.NewRequest("PUT", fmt.Sprintf("%s%s", c.Url, fmt.Sprintf("/api/v1/object_manager_attributes/%d", objectID)), o)
 	if err != nil {
-		return &object, err
+		return object, err
 	}
 
-	if err = c.SendWithAuth(req, &object); err != nil {
-		return &object, err
+	if err = c.SendWithAuth(req, object); err != nil {
+		return object, err
 	}
 
-	return &object, nil
+	return object, nil
 }
 
 func (c *Client) ObjectExecuteDatabaseMigration() error {
